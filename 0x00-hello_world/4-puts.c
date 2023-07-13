81d15b036cd997b13056fdbb852a1a64d75a61a2
IA-LINK <jyc.kom@gmail.com>
2023-07-13 08:32:38 -0700

    My 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+/**
+  * main - A Program that print a line with puts function
+* return: 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
