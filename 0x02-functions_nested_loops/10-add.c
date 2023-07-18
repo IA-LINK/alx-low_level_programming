f9c089c200b89b9c10bd08949151d86327a3dad5
IA-LINK <jyc.kom@gmail.com>
2023-07-18 16:46:13 -0700

    My add.c file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,10 +6,10 @@
  * Return: Always 0.
  */
 
-int add(int a, int b);
+int add(int, int);
 
 int main() {
-    int num1 =89;
+    int num1 = 89;
     int num2 = 9;
     int result = add(num1, num2);
     printf("The sum of %d and %d is %d\n", num1, num2, result);
