323a9c079d5b924ff0868631fc3b40d7474ca8dd
IA-LINK <jyc.kom@gmail.com>
2023-07-18 17:19:23 -0700

    My 10-add.c file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -9,12 +9,7 @@
  * Return: Integer value
  */
 
-int add(int a, int b);
-
-int main() {
-    int num1 = 10;
-    int num2 = 5;
-    int result = add(num1, num2);
-    printf("The sum of %d and %d is %d\n", num1, num2, result);
-    return 0;
+int add(int n1, int n2)
+{
+return (n1 + n2);
 }
