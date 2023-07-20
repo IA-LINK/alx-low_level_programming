1b7016d15530b1401504d16ff227434a85b356b5
IA-LINK <jyc.kom@gmail.com>
2023-07-20 15:28:41 -0700

    My prime factors file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,97 +1,68 @@
+#include <math.h>
 #include "main.h"
 
 /**
- *_print100_999 - print 100 - 999 digit
- *@n: parameter pass
- * Return: void
+ * power - exponents
+ * @base: base
+ * @exp: exponent
+ * Return: result (int)
  */
-void _print100_999(int n)
+
+int  power(int base, int exp)
 {
-	_putchar((n / 100) + '0');
-	_putchar(((n % 101) / 10) + '0');
-	_putchar(((n % 100) % 10) + '0');
+	int i, num;
+
+	num = 1;
+	for (i = 0; i < exp; ++i)
+		num *= base;
+
+	return (num);
 }
 
 /**
- * _print1000_9999 - print 1000 to 9999 digit
- *@n: parameter pass
- * Return: void
- */
-void _print1000_9999(int n)
-{
-	_putchar((n / 1000) + '0');
-	_putchar(((n % 1000) / 100) + '0');
-	_putchar((((n % 1000) % 100) / 10) + '0');
-	_putchar((((n % 1000) % 100) % 10) + '0');
-}
-
-/**
- * negative - function that print the negative integer
- * @n: parameter pass
+ * print_number - prints an integer
+ * @n: number to print
  * Return void
  */
-void negative(int n)
-{
-	if (n >= -9 && n < 0)
-	{
-		_putchar('-');
-		n = -n;
-		_putchar(n + '0');
-	}
-	if (n >= -99 && n < -9)
-	{
-		_putchar('-');
-		n = -n;
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	if (n >= -999 && n < -99)
-	{
-		_putchar('-');
-		n = -n;
-		_print100_999(n);
-	}
-	if (n >= -9999 && n < -999)
-	{
-		_putchar('-');
-		n = -n;
-		_print1000_9999(n);
-	}
-}
-/**
- * positive - function to print out integer
- *@n: parameter pass
- * Return: void
- */
-void positive(int n)
-{
-	if (n >= 0 && n <= 9)
-	{
-		_putchar(n + '0');
-	}
-	if (n >= 10 && n <= 99)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	if (n >= 100 && n <= 999)
-	{
-		_print100_999(n);
-	}
-	if (n >= 1000 && n <= 9999)
-	{
-		_print1000_9999(n);
-	}
-}
-/**
- * print_number - function that print an integer
- * @n: parameter pass
- *Return: void
- */
+
 void print_number(int n)
 {
+	int negative = 0;
+	int digit;
+	int divisor;
+	int begin = 0;
+	int place = 10;
+
 	if (n < 0)
-		negative(n);
-	else if (n >= 0)
-		positive(n);
+	{
+		negative = 1;
+		n = n * -1;
+	}
+	while (place >= 0)
+	{
+		/*divisor = pow(10, place);*/
+		divisor = power(10, place);
+		digit = ((n / divisor) % 10);
+		if (digit == 0 && begin == 0)
+		{
+			place--;
+		}
+		else if (digit != 0 && begin == 0)
+		{
+			begin = 1;
+			if (negative == 1)
+				_putchar('-');
+			_putchar('0' + digit);
+			place--;
+		}
+		else
+		{
+			_putchar('0' + digit);
+			place--;
+		}
+	}
+	if (digit == 0 && divisor == 1)
+	{
+		_putchar(48);
+	}
 }
