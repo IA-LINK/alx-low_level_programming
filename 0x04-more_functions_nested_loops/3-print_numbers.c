76272001634eb4ae91869ebae454abd9cb34e68a
IA-LINK <jyc.kom@gmail.com>
2023-07-20 12:43:48 -0700

    My 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number from 0 - 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 38; n <= 47; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
