acf77e302d3506d7cc79d5eab9036377e54179c2
IA-LINK <jyc.kom@gmail.com>
2023-07-24 09:12:29 -0700

    My 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that take pointer value of 98
+ *
+ * Description: found for function updateValueTo98
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
