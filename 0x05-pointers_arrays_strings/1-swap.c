8e935ebaa50ff2aa5bb23ec60930e5ee5538a895
IA-LINK <jyc.kom@gmail.com>
2023-07-24 10:03:51 -0700

    My 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int -  swap the values of two integers
+ * and update the value it points to 98
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
