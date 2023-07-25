b3569ea3bb67f0f67ce549baf4a7f26e7866801f
IA-LINK <jyc.kom@gmail.com>
2023-07-25 02:58:32 -0700

    My 3-puts.c update file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts -  Return the length of a string.
+ * @str: Check value string.
+ * Return: lnt
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
