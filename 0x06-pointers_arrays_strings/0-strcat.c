620c51137954f79703b59967cec463854c20e749
IA-LINK <jyc.kom@gmail.com>
2023-07-26 09:37:24 -0700

    My 0-strcat.c file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - Funtion string.
+ * @dest: destiny value.
+ * @src: String value.
+ * Return: dest.
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, src_lnt, dest_lnt; src_lnt = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		src_lnt++;
+		dest_lnt = 0;
+	}
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest_lnt++;
+	}
+	for (i = 0; i <= src_lnt; i++)
+	{
+		dest[dest_lnt + i] = src[i];
+	}
+	return (dest);
+}
