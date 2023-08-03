1d296c3acdd04b942c6c04aa971a67c5942113e9
IA-LINK <jyc.kom@gmail.com>
2023-08-03 09:19:49 -0700

    My 0-puts_recursion.c file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
 void _puts_recursion(char *s);
+int _putchar(char);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,5 +8,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-@endif
