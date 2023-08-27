98a7b6f742b61feb6cdff4445d6179fd3bafcef2
IA-LINK <jyc.kom@gmail.com>
2023-08-27 13:31:25 -0700

    1-list_len.c file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len -  returns the number of elements in a linked list_t list.
+ * @h: Adress of the head of the linked list
+ * Return: Unsigned int with the number of nodes printed
+ */
+size_t list_len(const list_t *h)
+{
+	size_t length = 0;
+
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		length++;
+		current =  current->next;
+	}
+	return (length);
+}
