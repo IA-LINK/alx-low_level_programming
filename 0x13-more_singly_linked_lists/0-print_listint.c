c5fab6d76b1f42997926366bfdd67106ee5a5fdb
IA-LINK <jyc.kom@gmail.com>
2023-08-28 06:50:42 -0700

    0-print_listint.c file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - print integer
+ * @h: the element containing the list
+ * Return: the node of the linked list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
