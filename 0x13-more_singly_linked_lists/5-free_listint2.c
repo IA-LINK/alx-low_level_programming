182c1dcd8240f67ac63e21e09388117629f270fd
IA-LINK <jyc.kom@gmail.com>
2023-08-28 14:17:07 -0700

    6-pop_listint.c file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	while (*head != NULL)
+	{
+		listint_t *temp = *head;
+		*head  = (*head)->next;
+		free(temp);
+	}
+	*head = (NULL);
+}
