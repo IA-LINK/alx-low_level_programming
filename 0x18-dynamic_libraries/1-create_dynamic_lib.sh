a08731507597356fc73c79d7120f49a92a5ca787
IA-LINK <jyc.kom@gmail.com>
2024-03-25 22:07:43 -0700

    Adding Task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Compile all .c files in the current directory into object files
-gcc -c -fPIC *.c
+gcc -wall -wextra -werror -pedantic -c -fPIC *.c
 # Create the dynamic library liball.so from the object files
 gcc -shared -o liball.so *.o
 # Clean up by removing the object files
-rm *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
