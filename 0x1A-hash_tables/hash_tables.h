4fefa59946fda9d5a2563885eee4d00a8c97161c
AI-LINK <jyc.kom@gmail.com>
2024-04-12 17:12:19 -0700

    0x1A. Hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -79,6 +79,7 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
+hash_table_t *hash_table_create(unsigned long int size);
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
