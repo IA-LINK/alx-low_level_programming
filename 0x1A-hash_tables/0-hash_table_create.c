00faceb62030f7e4f86bbaf9ee2282a52ec15812
AI-LINK <jyc.kom@gmail.com>
2024-04-13 13:43:03 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,31 +1,29 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_create - creates a hash table.
-  * @size: size of the hash table to create.
-  * Return: a pointer to the hash table.
-  */
-
+ * hash_table_create- create hash table.
+ * @size: the size of ther hash table
+ * Return: the table else NULL
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *table;
 	unsigned long int i;
-	hash_table_t *new_hashtable;
 
-	if (size <= 0)
-		return (NULL);
-
-	new_hashtable = malloc(sizeof(hash_table_t));
-	if (!new_hashtable)
-		return (NULL);
-
-	new_hashtable->size = size;
-	new_hashtable->array = malloc(size * sizeof(hash_node_t *));
-	if (!new_hashtable->array)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 	{
-		free(new_hashtable);
+		return (NULL);
+	}
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-		new_hashtable->array[i] = NULL;
-	return (new_hashtable);
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
 }
